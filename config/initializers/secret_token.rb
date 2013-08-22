01cb181ce20a5345db8130e1adb19c8b8a188d1a
gk <raistlin916@gmail.com>
2013-08-22 05:46:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FisrtApp::Application.config.secret_key_base = '25ce398127a1cdac5a5012d9859560964ac6ec2a0a1d0adaca8ae2a296cec05233e8ab7e880c250e3f3c8a78db959c6212ea8ab0f7671499d8df9454421983d8'
